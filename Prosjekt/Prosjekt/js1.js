732a6f34e46ab94177c5a0d55b90d346d53ee42b
sajain1 <31128580+sajain1@users.noreply.github.com>
2018-12-02 22:01:53 +0100

    Add files via upload

--- Prosjekt/Prosjekt/js1.js
+++ Prosjekt/Prosjekt/js1.js
@@ -1,5 +1,12 @@
-var spaceinvaders = document.getElementById("spaceinvaders")
+var spaceinvaders = document.getElementById("spaceinvaders");
 
-function hover() {
+spaceinvaders.onmouseover = function (event) {
+    spaceinvaders.style.backgroundImage = `url("https://i.gyazo.com/a08e4e733a325b1165f2fe81d393f912.gif")`;
+    console.log("hei")
+};
 
-}
+spaceinvaders.onmouseleave = function (event) {
+    spaceinvaders.style.backgroundImage = `url("spaceinvaders.png")`;
+    console.log("hei")
+};
+
